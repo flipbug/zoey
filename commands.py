55d292cdc78fb4a48683f2e3cada77c3ead5f2cd
Daniel Milenkovic <daniel.milenkovic@flipbug.ch>
2015-11-05 21:29:18 +0100

    i don't like long lines

--- commands.py
+++ commands.py
@@ -42,7 +42,8 @@ class SpeakCommand(Command):
 
 class SingCommand(Command):
     def  execute(self):
-         os.system("say -v cellos \"Doo da doo da dum dee dee doodly doo dum dum dum doo da doo da doo da doo da doo da doo da doo\"")
+         os.system("say -v cellos \"Doo da doo da dum dee dee doodly doo dum \
+            dum dum doo da doo da doo da doo da doo da doo da doo\"")
 
 class ItunesCommand(Command):
     def execute(self):
