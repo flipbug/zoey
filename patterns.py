32019a7822fa3aeb6bff960d7091c1564108f37a
Daniel Milenkovic <daniel.milenkovic@flipbug.ch>
2015-11-06 14:14:30 +0100

    add itunes as keyword

--- patterns.py
+++ patterns.py
@@ -1,11 +1,11 @@
 # a list of all available commands and the corresponding regex pattern
 PATTERNS = {
     'exit': "^(exit|quit|:q)",
-    'help': "^(help|man)",
+    'help': "^(help|man|-h)",
     'info': "^(info|who are you)",
     'vimeo': "^show.*video",
     'speak': "^speak",
     'sing': "^sing",
-    'itunes': "^(play|stop|next|previous)",
+    'itunes': "^(itunes|play|stop|next|previous)",
     'test': "^test"
 }
